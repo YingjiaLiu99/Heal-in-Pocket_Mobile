564fc2348581ec5d3625916c5b22a852b98c9cfd
yil023 <yil023@ucsd.edu>
2023-05-01 23:56:51 -0400

    update the login related files and structure

--- PocketHealth/App.js
+++ PocketHealth/App.js
@@ -3,9 +3,9 @@ import React from 'react';
 import { StyleSheet, View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import HomeScreen from './src/screens/HomeScreen';
-import LoginScreen from './src/screens/LoginScreen';
-import SignUpScreen from './src/screens/SignUpScreen'
+import LoginScreen from './screens/LoginScreen/LoginScreen';
+import SignUpScreen from './screens/LoginScreen/SignUpScreen'
+import WelcomeScreen from './screens/LoginScreen/WelcomeScreen';
 
 const Stack = createStackNavigator();
 
@@ -14,7 +14,7 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Welcome" component={WelcomeScreen} />
         <Stack.Screen name="Login" component={LoginScreen} />
         <Stack.Screen name="SignUp" component={SignUpScreen} />
       </Stack.Navigator>
---
+++ PocketHealth/screens/LoginScreen/SignUpScreen.js
@@ -0,0 +1,57 @@
+import React, { useState } from 'react';
+import { StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import styles from './styles.js';
+
+export default function SignUpScreen() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+/// The Backend code goes here
+  const handleSignUp = () => {
+    if (!email || !password || !confirmPassword) {
+      setErrorMessage('Please fill in all fields');
+    } else if (password !== confirmPassword) {
+      setErrorMessage('Passwords do not match');
+    } else {
+      // Call API to create user account
+      console.log('Sign up successful');
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Sign Up</Text>
+      {errorMessage ? <Text style={styles.error}>{errorMessage}</Text> : null}
+      <TextInput
+        style={styles.input}
+        placeholder="Email"
+        onChangeText={(text) => setEmail(text)}
+        value={email}
+        keyboardType="email-address"
+        autoCapitalize="none"
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Password"
+        onChangeText={(text) => setPassword(text)}
+        value={password}
+        secureTextEntry={true}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Confirm Password"
+        onChangeText={(text) => setConfirmPassword(text)}
+        value={confirmPassword}
+        secureTextEntry={true}
+      />
+      <TouchableOpacity style={styles.button} onPress={handleSignUp}>
+        <Text style={styles.buttonText}>Sign Up</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+
+
+
