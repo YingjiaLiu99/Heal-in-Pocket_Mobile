0b934f03e48f6df1ffb8efd6d17ab58ab1839fa5
yil023 <yil023@ucsd.edu>
2023-05-09 12:09:01 -0400

    remove the goBack button on the chatePage and MyRecordPage

--- PocketHealth/screens/ChatRoom/ChatMainScreen.js
+++ PocketHealth/screens/ChatRoom/ChatMainScreen.js
@@ -1,7 +1,17 @@
 import React from 'react';
 import { View } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+
 
 const ChatMainPage = () => {
+
+  const navigation = useNavigation();
+  React.useLayoutEffect(() => {
+    navigation.setOptions({
+      headerLeft: null
+    });
+  }, [navigation]);
+
   return (
     <View />
   );
