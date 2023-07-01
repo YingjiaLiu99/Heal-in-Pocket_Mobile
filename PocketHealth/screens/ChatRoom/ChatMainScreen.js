176cabe02966d4a04af83315c88623b27ea3befc
WenyuanChen1326 <cwyuan1010@gmail.com>
2023-07-01 14:26:17 -0700

    added provider login signup and forget password

--- PocketHealth/screens/ChatRoom/ChatMainScreen.js
+++ PocketHealth/screens/ChatRoom/ChatMainScreen.js
@@ -1,19 +1,38 @@
 import React from 'react';
-import { View } from 'react-native';
+// import { StyleSheet, Text, View, TouchableOpacity, ScrollView } from "react-native";
+import { StyleSheet, Text, View, TouchableOpacity, ScrollView, Image } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
+import ProviderList from "./../HomeScreen/components/ProviderList";
+import styles from "./styles"
+import ChatPage from './ChatPage';
 
 
 const ChatMainPage = () => {
 
-  const navigation = useNavigation();
-  React.useLayoutEffect(() => {
-    navigation.setOptions({
-      headerLeft: null
-    });
-  }, [navigation]);
+  // const navigation = useNavigation();
+  // React.useLayoutEffect(() => {
+  //   navigation.setOptions({
+  //     headerLeft: null
+  //   });
+  // }, [navigation]);
 
   return (
-    <View />
+    <View>
+      <ScrollView contentContainerStyle={styles.container}>             
+        {/* <Button onPress={() => performSwitch()} title='Switch to patient response'/> */}
+        <View>
+        <Text style={styles.text}>ProviderList</Text>
+        <ProviderList></ProviderList>
+        <Text style={styles.text}>Chat</Text>
+        <ChatPage style={styles.chatPage}></ChatPage>
+        </View>
+
+
+        </ScrollView>
+    </View>
+
+    
+   
   );
 };
 
