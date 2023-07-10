45e495c8c5689b9c68826ba25da151acaf2ab0db
YingjiaLiu99 <yliu4033@gmail.com>
2023-07-10 16:31:18 -0700

    Finish the homescreen annoucement

--- PocketHealth/screens/patient_screens/HomeScreen/styles.js
+++ PocketHealth/screens/patient_screens/HomeScreen/styles.js
@@ -2,31 +2,33 @@ import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    padding: 20,
+    flex: 1    
   },
   heading: {
     fontSize: 30,
     textAlign: 'left',
-    marginBottom: 20,
+    marginTop: 20,
+    marginBottom: 10,
+    marginLeft:20
   },
-  announcementContainer: {
-    flex: 1,
+  ButtonOuterContainer: {    
     borderColor: '#7C7C7C',
     borderWidth: 1,
     marginBottom: 20,
+    marginHorizontal:20,
     padding: 10,
     justifyContent: 'center',
     alignItems: 'center',
     borderRadius: 10,
+    height:150,
   },
-  announcementText: {
-    fontSize: 30,
+  ButtonNotesText: {
+    fontSize: 25,
     textAlign: 'center',
   },
   buttonContainer: {
-    height: 40,
-    width: '50%',
+    height: 50,
+    width: '70%',
     marginVertical: 10,
     alignItems: 'center',
     justifyContent: 'center',
@@ -37,57 +39,8 @@ const styles = StyleSheet.create({
   },
   buttonText: {
     color: '#fff',
-    fontSize: 15,
-  },
-  dialogueContainer: {
-    flex: 2,
-    borderWidth: 1,
-    borderRadius: 10,
-    marginBottom: 20,
-    borderColor: '#7C7C7C',
-    padding: 10,
-  },
-  dialogueItemContainer: {
-    marginBottom: 10,
-  },
-  dialogueLeft: {
-    alignSelf: 'flex-start',
-    marginRight: 10,
-    padding: 10,
-    backgroundColor: '#E1E1E1',
-    borderRadius: 10,
-  },
-  dialogueRight: {
-    alignSelf: 'flex-end',
-    marginLeft: 10,
-    padding: 10,
-    backgroundColor: '#D3F7FE',
-    borderRadius: 10,
-  },
-  dialogueTextLeft: {
-    fontSize: 18,
-    textAlign: 'left',
-  },
-  dialogueTextRight: {
-    fontSize: 18,
-    textAlign: 'right',
-  },
-  dialoguePersonLeft: {
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    fontSize: 14,
-    color: '#555555',
-    marginBottom: 5,
-  },
-  dialoguePersonRight: {
-    position: 'absolute',
-    top: 0,
-    right: 0,
-    fontSize: 14,
-    color: '#555555',
-    marginBottom: 5,
-  },
+    fontSize: 20,
+  },  
 });
 
 export default styles;
