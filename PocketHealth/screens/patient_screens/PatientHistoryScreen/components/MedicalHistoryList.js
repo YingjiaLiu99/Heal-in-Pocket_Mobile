5cf6b49924d4be10220bf60f14155ccbb5be6207
YingjiaLiu99 <yliu4033@gmail.com>
2023-07-09 18:06:21 -0700

    finished the patient record section

---
+++ PocketHealth/screens/patient_screens/PatientHistoryScreen/components/MedicalHistoryList.js
@@ -0,0 +1,50 @@
+import React, { useState } from 'react';
+import { View, TouchableOpacity, StyleSheet, Text } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
+
+import BigShowcaseBoxWithLabel from '../../../../components/BigShowcaseBoxWithLabel';
+
+
+const MedicalHistoryList = ({ title, items, width }) => {
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  return (
+    <View style={{ width: width}}>
+      <TouchableOpacity 
+        style={styles.header} 
+        onPress={() => setIsExpanded(!isExpanded)}
+      >
+        <Text style={styles.headerText}>{title}</Text>
+        <Icon name={isExpanded ? 'chevron-up' : 'chevron-down'} size={24} color="black" />
+      </TouchableOpacity>
+
+      {isExpanded && (
+        <View style={ {alignItems: 'center'} }>
+          {items.map((item, index) => {            
+            return <BigShowcaseBoxWithLabel key={index} {...item} width={width} />;
+          })}
+        </View>
+      )}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  header: {
+    backgroundColor: '#C5D1F9',
+    padding: 15,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    borderWidth: 2,
+    borderColor: '#C5D1F9',
+    borderRadius: 15,
+    marginBottom: 10,
+  },
+  headerText: {
+    fontSize: 16,
+    fontWeight: '500',
+  },
+});
+
+export default MedicalHistoryList;
