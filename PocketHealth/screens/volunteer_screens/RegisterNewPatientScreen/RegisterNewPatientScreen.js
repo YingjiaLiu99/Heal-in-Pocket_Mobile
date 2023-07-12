f71fccfb70433b336a0294f24a8d1ec76defe314
YingjiaLiu99 <yliu4033@gmail.com>
2023-07-11 19:08:00 -0700

    set up volunteer homescreen

---
+++ PocketHealth/screens/volunteer_screens/RegisterNewPatientScreen/RegisterNewPatientScreen.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Text, View, TouchableOpacity, } from "react-native";
+
+export default function RegisterNewPatient_volunteer({route, navigation}) {    
+    return(
+        <View>
+
+        </View>
+    );
+};
