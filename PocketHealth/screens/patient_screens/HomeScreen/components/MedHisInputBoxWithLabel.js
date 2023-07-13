48f7c4fb475f19335e0014aea64d19db6c521d41
WenyuanChen1326 <cwyuan1010@gmail.com>
2023-07-13 22:40:27 +0800

    changed the style

--- PocketHealth/screens/patient_screens/HomeScreen/components/MedHisInputBoxWithLabel.js
+++ PocketHealth/screens/patient_screens/HomeScreen/components/MedHisInputBoxWithLabel.js
@@ -1,3 +1,69 @@
+// // import React from 'react';
+// // import { View, Text, StyleSheet, TextInput, ScrollView } from 'react-native';
+
+// // const MedHisInputBoxWithLabel = ({ label, value, unit, width, onChangeText }) => {
+// //   return (
+// //     <View style={[styles.boxContainer, { width: width }]}>
+// //       <View style={styles.valueContainer}>
+// //         <View style={styles.labelValueContainer}>
+// //           <Text style={styles.boxLabel}>{label}</Text>
+// //           <ScrollView style={styles.scrollView}>
+// //             <TextInput
+// //               style={styles.boxField}
+// //               value={value}
+// //               onChangeText={onChangeText}
+// //               multiline
+// //             />
+// //           </ScrollView>
+// //           <Text style={styles.unit}>{unit}</Text>
+// //         </View>
+// //       </View>
+// //     </View>
+// //   );
+// // };
+
+// // const styles = StyleSheet.create({
+// //   boxContainer: {
+// //     marginBottom: 10,
+// //   },
+// //   valueContainer: {
+// //     flexDirection: 'row',
+// //     justifyContent: 'space-between',
+// //     borderWidth: 2,
+// //     borderColor: '#7C7C7C',
+// //     backgroundColor: '#FFFFFF',
+// //     borderRadius: 15,
+// //     paddingHorizontal: 10,
+// //     paddingTop: 5,
+// //   },
+// //   labelValueContainer: {
+// //     flex: 1,
+// //     minHeight: 60,
+// //     maxHeight: 200,
+// //   },
+// //   boxLabel: {
+// //     fontSize: 15,
+// //     fontWeight: '300',
+// //     marginRight: 10,
+// //   },
+// //   boxField: {
+// //     fontSize: 20,
+// //     maxHeight: 100,  // Modify as per your requirement
+// //     maxWidth: '90%', // Modify as per your requirement
+// //   },
+// //   scrollView: {
+// //     maxHeight: 200, // Modify as per your requirement
+// //     maxWidth: '90%', // Modify as per your requirement
+// //   },
+// //   unit: {
+// //     fontSize: 20,
+// //     alignSelf: 'center',
+// //     marginTop: 18,
+// //   },
+// // });
+
+// // export default MedHisInputBoxWithLabel;
+
 // import React from 'react';
 // import { View, Text, StyleSheet, TextInput, ScrollView } from 'react-native';
 
@@ -7,7 +73,7 @@
 //       <View style={styles.valueContainer}>
 //         <View style={styles.labelValueContainer}>
 //           <Text style={styles.boxLabel}>{label}</Text>
-//           <ScrollView style={styles.scrollView}>
+//           <ScrollView>
 //             <TextInput
 //               style={styles.boxField}
 //               value={value}
@@ -38,8 +104,8 @@
 //   },
 //   labelValueContainer: {
 //     flex: 1,
-//     minHeight: 60,
-//     maxHeight: 200,
+//     minHeight: 100,
+//     maxHeight: 200, // This sets a fixed height for the TextInput box
 //   },
 //   boxLabel: {
 //     fontSize: 15,
@@ -48,11 +114,6 @@
 //   },
 //   boxField: {
 //     fontSize: 20,
-//     maxHeight: 100,  // Modify as per your requirement
-//     maxWidth: '90%', // Modify as per your requirement
-//   },
-//   scrollView: {
-//     maxHeight: 200, // Modify as per your requirement
 //     maxWidth: '90%', // Modify as per your requirement
 //   },
 //   unit: {
@@ -63,6 +124,7 @@
 // });
 
 // export default MedHisInputBoxWithLabel;
+
 
 import React from 'react';
 import { View, Text, StyleSheet, TextInput, ScrollView } from 'react-native';
@@ -104,8 +166,8 @@ const styles = StyleSheet.create({
   },
   labelValueContainer: {
     flex: 1,
-    minHeight: '100%',
-    maxHeight: '200%', // This sets a fixed height for the TextInput box
+    minHeight: 60,
+    maxHeight: 200, // This sets a fixed height for the TextInput box
   },
   boxLabel: {
     fontSize: 15,
@@ -124,5 +186,3 @@ const styles = StyleSheet.create({
 });
 
 export default MedHisInputBoxWithLabel;
-
-
