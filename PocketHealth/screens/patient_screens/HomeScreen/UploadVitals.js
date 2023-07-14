67fb307871e738754cb308aed8a346286bf54650
YingjiaLiu99 <yliu4033@gmail.com>
2023-07-13 23:14:49 -0700

    patient's upload record is done!

--- PocketHealth/screens/patient_screens/HomeScreen/UploadVitals.js
+++ PocketHealth/screens/patient_screens/HomeScreen/UploadVitals.js
@@ -1,8 +1,10 @@
 import React, { useState } from 'react';
-import { Text, View, TouchableOpacity, StyleSheet } from 'react-native';
-import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
+import { Text, View, TouchableOpacity } from 'react-native';
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
 
 import VitalsInputBoxWithLabel from './components/VitalsInputBoxWithLabel';
+import styles from './styles';
+
 
 export default function UploadVitals({ navigation }) {
   const labelProperties = {
@@ -34,9 +36,13 @@ export default function UploadVitals({ navigation }) {
   };
 
   return (
-    <KeyboardAwareScrollView style={styles.container}>
-      <Text style={styles.title}>Upload Vitals</Text>
-      <View style={styles.content}>
+    <KeyboardAwareScrollView contentContainerStyle={styles.container}>
+      
+      <View style={{marginTop: 20,marginBottom:30,width:'100%'}}>
+        <Text style={{fontSize:35, fontWeight:400}}>Update My Vitals</Text>          
+      </View>
+
+      <View style={{width:"100%"}}>
         {Object.entries(labelProperties).map(([label, properties], index) => (
           <VitalsInputBoxWithLabel
             key={index}
@@ -48,50 +54,19 @@ export default function UploadVitals({ navigation }) {
           />
         ))}
       </View>
-      <View style={styles.buttonContainer}>
+            
+      <View style={{width:'80%',alignItems:'center',marginTop:0,marginBottom:0}}>
         <TouchableOpacity style={styles.button} onPress={handleReviewSubmit}>
           <Text style={styles.buttonText}>Review and Submit</Text>
         </TouchableOpacity>
       </View>
-      <View style={styles.buttonContainer}>
+
+      <View style={{width:'80%',alignItems:'center',marginTop:0,marginBottom:0}}>
         <TouchableOpacity style={styles.button} onPress={handleSkip}>
           <Text style={styles.buttonText}>Skip</Text>
         </TouchableOpacity>
       </View>
+
     </KeyboardAwareScrollView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    paddingHorizontal: 20,
-    paddingTop: 20,
-  },
-  title: {
-    fontSize: 34,
-    fontWeight: 'bold',
-    marginBottom: 20,
-  },
-  content: {
-    marginTop: 20,
-    flex: 1,
-  },
-  buttonContainer: {
-    flexDirection: 'row',
-    justifyContent: 'center',
-    marginTop: 10,
-  },
-  button: {
-    backgroundColor: '#395BCD',
-    borderRadius: 10,
-    paddingVertical: 10,
-    paddingHorizontal: 10,
-    alignItems: 'center',
-    width: '70%',
-  },
-  buttonText: {
-    color: '#fff',
-    fontSize: 20,
-  },
-});
