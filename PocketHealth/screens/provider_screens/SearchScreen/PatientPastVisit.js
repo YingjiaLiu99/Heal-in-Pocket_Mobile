6c9c39869f29e0f7bfbee31327bad6158818bb20
Zihan Qu <57647548+QzhStarkInternational@users.noreply.github.com>
2023-07-30 15:34:20 +0800

    past record has bug
    In Paitent past visit, the foldable list can not display.

--- PocketHealth/screens/provider_screens/SearchScreen/PatientPastVisit.js
+++ PocketHealth/screens/provider_screens/SearchScreen/PatientPastVisit.js
@@ -1,11 +1,214 @@
-import React from "react";
-import { Text, View } from 'react-native';
+import React, { useState, useRef } from "react";
+import { Text, View, TouchableOpacity, FlatList, TouchableWithoutFeedback, Keyboard, ScrollView} from "react-native";
+import { SearchBar } from '@rneui/themed';
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
+//import { useRoute } from '@react-navigation/native';
 
 import styles from './styles.js';
+import SearchPastVistReport from "./components/SearchPastVistReport.js"
 
-export default function PatientPastVisit( {navigation} ) {
 
-    return(
-        <Text>Hello World!</Text>
-    );
+
+
+
+export default function PatientPastVisit( {route, navigation} ) {
+    //const route = useRoute();
+    const { user } = route.params;
+
+    const [errorMessage, setErrorMessage]= useState("");
+    const [lockerNewFeature, setLocker] = useState(false);
+
+    const providerReport = [
+      {   
+          label: 'Reason For consultation', 
+          value: 'Left hand wound leaking pus'
+      },
+      {   
+          label: 'Assessment', 
+          value: 'Left hand wound infected, cleaned wound with saline and applied antibiotic ointment. Antibiotic ointment samples given to patients.'
+      },
+      {   
+          label: 'Future Plan', 
+          value: 'Use antibiotic ointment twice a day. Come back to street corner care next week.'
+      },
+  ];
+
+  const medicalHistory = [
+      {   
+          label: 'Chronic Illness', 
+          value: ' high blood pressure, diabetes'
+      },
+      {   
+          label: 'Current Medication', 
+          value: 'Metoprolol'
+      },
+      {
+          label: 'Allergies', 
+          value: 'Sulfa'
+      },        
+  ];
+
+  const vitalData = [
+      // {label: 'Pain Level(0~10,0-no pain,10-worst pain)', value: '8'},
+      {label: 'Temp', value: '99'},
+      {label: 'Oxygen', value: '98'},
+      {label: 'Pulse', value: '70'},
+      {label: 'BP', value: '120/80'},
+      {label: 'Glucose', value: '110'},        
+  ];
+
+  const DateOfService = {label: 'Date of Service', value: '2023-07-21'};
+  const location = {label: 'Care Location', value: 'Street Corner Care'};
+
+
+  const dummyNote = {
+    ProviderNote: providerReport,
+    medicalHistory: medicalHistory,
+    vitals: vitalData
+  };
+
+  const FullData = [
+    {
+        date: "Nov, 12, 2022",
+        record: [
+            { time: "10:00 am", visitNote: dummyNote},
+            { time: "12:00 pm", visitNote: dummyNote},
+        ],    
+    },   
+
+    {
+        date: "Dec, 11, 2022",
+        record: [
+            { time: "9:00 am", visitNote: dummyNote},
+        ],            
+    }
+  ];
+
+
+  // Assume all user has same fullData, data retrieve by user id. 
+  // const databaseByUser = [
+  //   {
+  //       name: "David Smith",
+  //       data: FullData 
+  //   },   
+
+  //   {
+  //     name: "Maria Rodriguez",
+  //     data: FullData          
+  //   },
+  // ];
+  
+
+
+
+  const handleSubmit = () => {
+      if (!lockerNewFeature) {
+        setErrorMessage("New Feature is Coming :) ");
+      }
+      else {
+        navigation.navigate('Create New Note');
+      }
+  }; 
+
+  const [expandedDates, setExpandedDates] = useState([]);
+
+  const toggleExpandedDate = (date) => {
+
+    // If we toggle before and retoggle it, it remove certain date
+    if (expandedDates.includes(date)) {
+      const index = expandedDates.indexOf(date);
+      const newExpandedDates = [...expandedDates];
+      newExpandedDates.splice(index, 1);
+      setExpandedDates(newExpandedDates);
+    } else {
+      setExpandedDates([...expandedDates, date]);
+    }
+  }
+
+
+  return (
+    <View style={styles.container}>
+        <View style={{
+          position: 'absolute', 
+          top: 0, 
+          left: 0, 
+          right: 0, 
+          padding: 10, 
+          backgroundColor: '#DDE5FD', 
+          zIndex: 999, 
+          elevation: 3, 
+          flexDirection: 'column',
+          justifyContent: 'space-between',
+          height:100
+        }}>
+
+          <View style={{ flexDirection: 'row' }}>
+            <Text style={{fontSize: 25, fontWeight: '500', width: '100%'}}>Name: {user.firstName} {user.lastName}</Text>
+          </View>
+          <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+            <Text style={{fontSize: 20, fontWeight: '400', width: '45%'}}>DOB: {user.dateOfBirth}</Text>
+            <Text style={{fontSize: 20, fontWeight: '400', width: '45%'}}>DOS: {DateOfService.value}</Text>
+          </View>
+          <View style={{ flexDirection: 'row' }}>
+            <Text style={{fontSize: 20, fontWeight: '400', width: '100%'}}>Site: {location.value}</Text>
+          </View>
+        </View>
+
+        {/* Change to user ID later */}
+        {/* <Text>{user.firstName} {user.lastName}</Text>
+        <Text>Date of Birth: {user.dateOfBirth}</Text> */}
+        
+
+        <FlatList>
+          contentContainerStyle={{ paddingTop: 100 }}
+          ListHeaderComponent={() => <Text>Patient Past Visit</Text>}
+          data={FullData}
+          keyExtractor={(item, index) => index.toString()}
+          renderItem={({ item }) => (
+            <View>
+                <TouchableOpacity 
+                    style={[styles.header, { backgroundColor: expandedDates.includes(item.date) ? 'white' : 'white' }]} 
+                      onPress={() => toggleExpandedDate(item.date)}>
+                  <Text style={styles.dateText}>{item.date}</Text>
+                  <Icon name={expandedDates.includes(item.date) ? 'chevron-up' : 'chevron-down'} size={24} color="black" />
+                </TouchableOpacity>
+                  {expandedDates.includes(item.date) && item.record.map((visit, index) => (
+                      <SearchPastVistReport
+                          key={index}
+                          time={visit.time}
+                          providerReport={visit.visitNote.providerReport}
+                          medicalHistory={visit.visitNote.medicalHistory}
+                          vitalData={visit.visitNote.vitalData}
+                          width={'95%'}
+                      />
+                  ))}
+            </View>
+          )}
+
+
+
+        </FlatList>
+      
+
+
+        {!lockerNewFeature ? <Text style={{color:'red', fontSize:18, marginBottom:10}}>{errorMessage}</Text> : null}
+
+        <View style={{width:'80%',alignItems:'center',marginTop:0,marginBottom:30}}>
+          <TouchableOpacity style={styles.button} onPress={handleSubmit}>
+            <Text style={styles.buttonText}>
+              {"Create New Note"}
+            </Text>
+          </TouchableOpacity>
+        </View>
+
+        
+        
+
+
+
+        
+      </View>
+
+        
+  );
 };
--- PocketHealth/screens/provider_screens/SearchScreen/styles.js
+++ PocketHealth/screens/provider_screens/SearchScreen/styles.js
@@ -1,6 +1,113 @@
 import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',      
+        justifyContent: 'flex-start',
+        flexDirection: 'column',
+        paddingHorizontal: 20,
+        paddingVertical:0,      
+        marginTop: 0,
+        marginHorizontal:0,
+        // backgroundColor:'#FFFFFF'
+      },
+      
+
+      containerBottom: {
+        flex: 1,
+        alignItems: 'center',      
+        justifyContent: 'flex-start',
+        flexDirection: 'column',
+        paddingHorizontal: 20,
+        paddingVertical:0,      
+        marginTop: 0,
+        marginHorizontal:0,
+        // backgroundColor:'#FFFFFF'
+      },
+      header: {
+        backgroundColor: '#C5D1F9',
+        padding: 15,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        borderWidth: 2,
+        borderColor: '#C5D1F9',
+        borderRadius: 15,
+        marginBottom: 10,
+      },
+
+      dateText: {
+        fontSize: 16,
+        fontWeight: '500',
+      },
+      
+      titleText: {
+        alignItems: 'center',      
+        fontSize: 45,
+        fontWeight: 400  
+      },
+
+      buttonContainer: {
+        height: 70,
+        width: '100%',
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginBottom:15
+      },
+      
+      button: {
+        height: 70,
+        width: '100%',      
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#395BCD',
+        paddingVertical: 5,
+        paddingHorizontal: 5,
+        borderRadius: 20
+      },
+      buttonText: {
+        color: '#fff',
+        fontSize: 25
+      },
+
+      userShowcase: {
+        height: 70,
+        width: '100%',
+        marginVertical: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#E4E3E9',
+        paddingVertical: 5,
+        paddingHorizontal: 5,
+        borderRadius: 15,             
+      },
+      userShowcaseText: {
+        color: '#000000',
+        fontSize: 18
+      },
+  
+      dropdownContainer: {
+        height: 400, // Adjust to suitable value
+        marginBottom: 40 // Add some space at the end of the list
+      },
+  
+      error: {
+        color: 'red',
+        marginBottom: 20
+      },  
+      confirmButton: {
+        backgroundColor: '#FF9248',
+        // ... other styles same as 'button' ...
+        height: 70,
+        width: '100%',      
+        alignItems: 'center',
+        justifyContent: 'center',
+        paddingVertical: 5,
+        paddingHorizontal: 5,
+        borderRadius: 20
+      },
 
     
 });
+export default styles;
