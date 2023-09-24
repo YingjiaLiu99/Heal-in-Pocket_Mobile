c0e214830f6d3b929e9d18dac07df31530b1b879
IvyChenhan <c6li@ucsd.edu>
2023-09-23 23:14:39 -0400

    updating the request correspond to frontend

--- Heal-in-Pocket/common/baseURL.js
+++ Heal-in-Pocket/common/baseURL.js
@@ -5,7 +5,7 @@ let baseURL;
 if (Platform.OS === 'android') {
     baseURL = 'http://10.0.2.2:5001/api/v1.0/';  // This IP is for Android emulator
 } else if (Platform.OS === 'ios') {
-    baseURL = 'http://<your computer local network>:5001/api/v1.0/';  // Replace with your computer's local network IP address
+    baseURL = 'http://35.3.216.16:5001/api/v1.0/';  // Replace with your computer's local network IP address
 } else if (Platform.OS === 'web') {
     baseURL = 'http://localhost:5001/api/v1.0/';
 }
